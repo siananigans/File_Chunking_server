d199c068d2b2ab38019a021da47cc03bccca9af0
lennos34 <sian.lennon34@mail.dcu.ie>
2020-04-06 21:52:07 +0100

    Testing Pycharm

--- src/server.py
+++ src/server.py
@@ -1,1 +1,19 @@
-from aoihttp
+from aiohttp import web
+from views import index
+
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+
+
+async def my_web_app():
+    app = web.Application()
+    app.add_routes([web.get('/', handle),
+                    web.get('/{name}', handle)])
+    return app
+
+if __name__ == '__main__':
+    my_web_app()
