654ff71d9952d9cc45e24c09d21c6165459f0f4e
lennos34 <sian.lennon34@mail.dcu.ie>
2020-04-06 22:24:32 +0100

    Queue view

--- src/views.py
+++ src/views.py
@@ -3,3 +3,6 @@ from aiohttp import web
 async def index(request):
     return web.Response(text="Hello World")
 
+async def queue(request):
+    return web.Request(text="This is where the queue goes.")
+
