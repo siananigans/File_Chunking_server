f579245bfc72dfc631cec884cd6e227cedb0067d
Sian Lennon <sianlennon109@gmail.com>
2020-04-14 00:08:28 +0100

    Update settings.py

--- src/ebdjango/ebdjango/settings.py
+++ src/ebdjango/ebdjango/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '25wh6gfwgkm!8wy$)o_q#h$tfm9^_%$vp50n%rkkbm#cc(230d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
